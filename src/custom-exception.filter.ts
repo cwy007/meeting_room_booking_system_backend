1e0314285b9c717e7d4cf9d696fbde647b96cda5
chanweiyan <chanweiyan007@gmail.com>
2026-05-16 11:16:24 +0800

    fix: return validation error

--- src/custom-exception.filter.ts
+++ src/custom-exception.filter.ts
@@ -8,10 +8,13 @@ export class CustomExceptionFilter<T extends HttpException> implements Exception
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
+    const res = exception.getResponse() as { message: string[] };
+    console.error('捕获到异常:', res);
+
     response.status(status).json({
       code: status,
       message: 'fail',
-      data: exception.message,
+      data: res.message?.join(', ') || exception.message,
     });
   }
 }
